c13cc2b1eca735272c5b55528bc9caf417c9f0b7
Erika <essnow@wpi.edu>
2018-10-01 14:52:05 -0400

    finished refactoring the screen changes

--- js/sceneHandler.js
+++ js/sceneHandler.js
@@ -2,23 +2,3 @@ function changeScreen(newScreen) {
     newScreen = newScreen + '.html';
     $('#initScreen').load(newScreen);
 }
-
-function goToRules() {
-    $('#initScreen').load('rulesScreen.html');
-}
-
-function goToGame() {
-    $('#initScreen').load('tutorialScreen.html');
-}
-
-function goToEnd() {
-    $('#initScreen').load('endScreen.html');
-}
-
-function goToMain() {
-    $('#initScreen').load('mainScreen.html');
-}
-
-function goToScores() {
-    $('#initScreen').load('scoreScreen.html');
-}
