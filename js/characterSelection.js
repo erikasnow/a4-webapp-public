fc450242b06e6b4fd02a95fb9bf9d26c78605565
Wheeleral <awheeler2@wpi.edu>
2018-10-01 14:40:52 -0400

    Char selection'

--- js/characterSelection.js
+++ js/characterSelection.js
@@ -1,6 +1,9 @@
 //clickable for each charcter
-function chooseCharacter(){
-
+function chooseCharacter(character){
+    console.log("character"+character)
+    window.character = character;
+    console.log(window.character)
+    displayCharacterDescription(character)
 }
 
 
@@ -10,7 +13,26 @@ function switchCharacter(){
 }
 
 //put character information in text above
-function displayCharacterDescription(){
-
+function displayCharacterDescription(character){
+    let textprompt = document.getElementById("textprompt");
+    let text;
+    switch (character) {
+        case "Ally":
+           text =  " Has a cat"
+            break;
+        case "Erika":
+            text= " A strong willed woman"
+            break;
+        case "Joan":
+            text =" Very thinker"
+            break;
+        case "Krysta":
+           text= " Very Observant"
+            break;
+        default:
+            break;
+    }
+    console.log(text)
+    textprompt.innerHTML = character+ text;
 }
 
