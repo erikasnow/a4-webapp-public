6e4744e0fa1632492816d8029c574e0055cd0e11
Erika <essnow@wpi.edu>
2018-10-01 16:36:20 -0400

    added inventory refresh for screen changes

--- js/sceneHandler.js
+++ js/sceneHandler.js
@@ -1,4 +1,20 @@
-function changeScreen(newScreen) {
+function changeScreen(newScreen, removeInventory) {
     newScreen = newScreen + '.html';
     $('#initScreen').load(newScreen);
+
+    if(removeInventory){
+        clearInventory();
+    }
+}
+
+function clearInventory() {
+    var cellsRow = document.getElementById("inventoryCells");
+    var cells = cellsRow.getElementsByTagName("td");
+
+    for(let i = 0; i < cells.length; i++){
+        console.log("entered for loop")
+        cells[i].innerHTML = '';
+    }
+
+    console.log("should have deleted all pictures from inventory");
 }
