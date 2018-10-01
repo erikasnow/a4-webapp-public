7d343210a68a6a16134b6e64642062f727c0ed30
Wheeleral <awheeler2@wpi.edu>
2018-10-01 14:16:56 -0400

    Switchin with new .html

--- js/sceneHandler.js
+++ js/sceneHandler.js
@@ -1,28 +1,36 @@
 initScreen();
 
 function initScreen() {
-    var link = document.querySelector('link[rel="import"]');
-    var content = link.import;
+   // var link = document.querySelector('link[rel="import"]');
+   // var content = link.import;
 
     // Grab DOM from warning.html's document.
-    var el = content.querySelector('.mainScreen');
+    //var el = content.querySelector('.mainScreen');
 
     //document.body.appendChild(el.cloneNode(true));
     //document.getElementById("screen").replaceChild(el.cloneNode(true), document.getElementById("initScreen"));
     //document.getElementById("screen").appendChild(el.cloneNode(true));
+          
+    //$('#initScreen').load('#mainScreen');
+           
+   // $('#initScreen').load('screens.html');
+
 }
 
 function goToRules() {
-    var link = document.querySelector('link[rel="import"]');
-    var content = link.import;
+   // var link = document.querySelector('link[rel="import"]');
+    //var content = link.import;
 
     // Grab DOM from screens.html's document.
-    var el = content.querySelector('.rulesScreen');
+   // var el = content.querySelector('.rulesScreen');
 
     //document.body.appendChild(el.cloneNode(true));
     //document.getElementById("screen").replaceChild(el.cloneNode(true), document.getElementById("mainScreen"));
-    document.getElementById("screen").appendChild(el.cloneNode(true));
-    document.getElementById("screen").removeChild(document.getElementById("screen").firstChild);
+   // document.getElementById("screen").appendChild(el.cloneNode(true));
+    //document.getElementById("screen").removeChild(document.getElementById("screen").firstChild);
+   // $(document).ready(function(){
+       $('#initScreen').load('scene1.html');
+   // });
 }
 
 function goToGame() {
