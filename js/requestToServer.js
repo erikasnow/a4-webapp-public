4d7a9e42851c1bd0e4020dcd65d7b05e1bf85e6d
unknown <jwong3@wpi.edu>
2018-10-03 02:02:46 -0400

    Added template html statements for score table

--- js/requestToServer.js
+++ js/requestToServer.js
@@ -3,8 +3,19 @@ var scoreReq = new XMLHttpRequest();
 scoreReq.onreadystatechange = function() {
     if (this.readState === 4 && this.status === 200) {
         var data = JSON.parse(this.responseText);
+
+        fillTable(data);
     }
 };
+
+function fillTable(data) {
+    var htmlTable = `${
+        data.map(data = function() {
+            `<tr><td>${data.name}</td><td>${data.score}</td></tr>`
+        }).join('')
+        }`;
+    document.getElementById("scoreTable").innerHTML += htmlTable;
+}
 
 function getScores() {
     scoreReq.open('GET', '/score');
